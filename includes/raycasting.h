50c5628cdcf006defe591b87ea7e1fb91c25c282
Tsunghao <tsunghao.chen12@gmail.com>
2024-08-27 18:54:04 +0200

    added sudo code for strip pixel put and ray_dist function

--- includes/raycasting.h
+++ includes/raycasting.h
@@ -6,12 +6,18 @@
 /*   By: tsuchen <tsuchen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 17:39:13 by tsuchen           #+#    #+#             */
-/*   Updated: 2024/08/27 17:46:24 by tsuchen          ###   ########.fr       */
+/*   Updated: 2024/08/27 18:37:26 by tsuchen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RAYCASTING_H
 # define RAYCASTING_H
+
+# define WIDTH 1280
+# define HEIGHT 720
+
+typedef struct s_vec	t_vec;
+typedef struct s_data	t_data;
 
 typedef struct s_image
 {
@@ -22,7 +28,8 @@ typedef struct s_image
 	int		endian;
 }	t_image;
 
-void	my_mlx_pixel_put(t_image *image, int x, int y, int color);
-
+void	rc_mlx_pixel_put(t_image *image, int x, int y, int color);
+void	rc_strip_pixel_put(t_image *image, int x, double ray_dist);
+double	rc_raydist(t_vec *ray, t_data *data);
 
 #endif
--- srcs/raycasting/raycasting.c
+++ srcs/raycasting/raycasting.c
@@ -6,16 +6,44 @@
 /*   By: tsuchen <tsuchen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 12:13:34 by tsuchen           #+#    #+#             */
-/*   Updated: 2024/08/27 17:43:20 by tsuchen          ###   ########.fr       */
+/*   Updated: 2024/08/27 18:40:43 by tsuchen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "raycasting.h"
 
-void	my_mlx_pixel_put(t_image *image, int x, int y, int color)
+void	rc_mlx_pixel_put(t_image *image, int x, int y, int color)
 {
 	char	*dst;
 
 	dst = image->addr + (y * image->line_length + x * (image->bpp / 8));
 	*(unsigned int *)dst = color;
 }
+
+void	rc_strip_pixel_put(t_image *image, int x, double ray_dist)
+{
+	int	y;
+	int	wall_height;
+
+	wall_height = ; // something reverse related to ray_dist
+	y = -1;
+	while (++y < HEIGHT)
+	{
+		if (y < (HEIGHT - wall_height) / 2)
+			rc_mlx_pixel_put(image, x, y, color_cieling);
+		else if (y > (HEIGHT + wall_height) / 2)
+			rc_mlx_pixel_put(image, x, y, color_floor);
+		else
+			rc_mlx_pixel_put(image, x, y, color_wall);
+	}
+}
+
+double	rc_raydist(t_vec *ray, t_data *data)
+{
+	double	dist;
+
+	dist = 0.0f;
+	
+	/* take an individual ray vector and calculate distance from P to Wall*/
+}
+
