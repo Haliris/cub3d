b6bb3f42fca1d8f3358251ca65f1c0613c9c35fe
Tsunghao <tsunghao.chen12@gmail.com>
2024-09-04 13:38:18 +0200

    Fixed segfault and potential leak when frames failed to load

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: tsuchen <tsuchen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 00:19:44 by tsuchen           #+#    #+#             */
-/*   Updated: 2024/09/04 12:28:56 by tsuchen          ###   ########.fr       */
+/*   Updated: 2024/09/04 13:37:03 by tsuchen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,6 +59,7 @@
 # define MOUSE_PRESS 4
 # define MOUSE_MOVE 6
 # define FILE_EXTENSION ".cub"
+# define FRAME_SCRIPT "./assets/animations/door_frame.txt"
 
 typedef enum e_texture		t_texture;
 typedef struct s_textdata	t_textdata;
